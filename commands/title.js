5dd58a14ee16d1e5dbef645223c77f3bb097c2e3
benryne <bryne@g.clemson.edu>
2020-09-08 12:24:18 -0500

    first ccommit

---
+++ commands/title.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+
+const queryAnime = async (payload) => {
+    try {
+        return await         
+        axios({
+            method: 'GET',
+            url: 'https://api.jikan.moe/v3/search/anime',
+            params: { q: payload, limit: '1' }
+        })
+    } catch (error) {
+        console.error(error)
+    }
+}
+  
+const getAnime = async (payload) => {
+    const anime = await queryAnime(payload)
+    console.log(anime.data.results);
+    const animeTitles = anime.data.results.map(function(a) {
+        return '> ' + a.title + 
+            '\naired: ' + a.start_date.slice(0,10) + ' - ' + a.end_date.slice(0,10) +
+            '\nrated: ' + a.rated +
+            '\nepisodes: ' + a.episodes +
+            '\nscore: ' + a.score + 
+            '\nmembers: ' + a.members +
+            '\n' + a.url;
+    });
+    
+    return animeTitles;
+}
+  
+
+module.exports = {
+    name: 'title',
+    description: 'returns info about anime title',
+    async execute (message, payload){
+        const t = await getAnime(payload);
+        message.channel.send(t);
+    }
+}
---
+++ index.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+const client = new Discord.Client();
+
+const prefix = 'anime!';
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+
+client.once('ready', () => {
+    console.log('online');
+})
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot ) return;
+
+    console.log(message.content + " " + message.content.length);
+
+    const args = message.content.slice(prefix.length,message.content.length).split(' ');
+    const command = args[0].toLowerCase();
+    const payload = args[1].toLowerCase();
+
+    console.log(command);
+    console.log(payload);
+
+    if(command == 'title') {
+        client.commands.get('title').execute(message,payload);
+    } else if(command == 'google') {
+        message.channel.send('https://google.com');
+    }
+})
+
+client.login('NzUxMTMzNDQ3NzY2MDE2MDUy.X1EpiQ.2KzCrL4eWsdYzGBtBhi3xHHtEVQ');
