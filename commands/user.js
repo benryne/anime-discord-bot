dab63446c6526e79a397e9955c30774d506ef029
benryne <bryne@g.clemson.edu>
2020-09-12 19:39:20 -0500

    added error and input control for user command

--- commands/user.js
+++ commands/user.js
@@ -10,7 +10,8 @@ const queryAnimeUser = async (payload) => {
                 url: `https://api.jikan.moe/v3/user/${user[0]}/animelist`
             })
         } catch (error) {
-            console.error(error)
+            message.channel.send('Problem with api try agian');
+            return;
         }
     }
     else {
@@ -21,16 +22,61 @@ const queryAnimeUser = async (payload) => {
                 url: `https://api.jikan.moe/v3/user/${user[0]}/animelist/${user[1]}`
             })
         } catch (error) {
-            console.error(error)
+            message.channel.send('Problem with api try agian');
+            return;
         }
     }
 }
+
+
+const testInput = async (message,payload) => {
+    const user = payload.split(' ');
+    if(user.length > 2) {
+        message.channel.send('Error: Too many arguments\n > User Commands: \n' +
+        'a!user [mal username]\n' +
+        'a!user [mal username] [watching,completed,on hold,dropped,plan to watch]\n\n');
+        return false;
+    }
+    if(user.length == 2) {
+        let a = user[1].toLowerCase();
+        if(a != 'watching' && a != 'completed' && a != 'on hold' && a != 'dropped' && a != 'ptw' && a != "plan to watch") {
+            message.channel.send('Error: Second argument was not correct\n > User Commands: \n' +
+            'a!user [mal username]\n' +
+            'a!user [mal username] [watching,completed,on hold,dropped,plan to watch]\n\n');
+            return false;
+        }
+    }
+    if(await testAnimeUserExists(message,payload) == undefined) {
+        message.channel.send('Error: Could not find user');
+        return false;
+    }
+    return true;
+}
+
+const testAnimeUserExists = async (message,payload) => {
+    const user = payload.split(' ');
+    try {
+        return await         
+        axios({
+            method: 'GET',
+            url: `https://api.jikan.moe/v3/user/${user[0]}/profile`
+        })
+    } catch (error) {
+        return;
+    }
+}
   
-const getAnimeUser = async (payload) => {
-    const user = await queryAnimeUser(payload);
-    console.log(user.data.anime);
+const getAnimeUser = async (message,payload) => {
+
+    if(await testInput(message,payload)) {
+        const user = await queryAnimeUser(payload);
+        formatAnimeTitlesAndSendMessage(message,user);
+    }
+}
+
+const formatAnimeTitlesAndSendMessage = (message,user) => {
+    if(user == undefined) return;
     const userAnimeTitles = user.data.anime.map(function(a) {
-        // remove entrires that are not tv or are continuing 
         if(a.watching_status == 1) {
             return '> ' + a.title + '\nWatching' + '\tScore: ' + a.score + '\tEpisodes: ' + a.watched_episodes +'/' + a.total_episodes + '\n';
         }
@@ -47,17 +93,15 @@ const getAnimeUser = async (payload) => {
             return '> ' + a.title + ' Plan To Watch' + '\n' ;
         }
     }); 
-    console.log(userAnimeTitles);
-    return userAnimeTitles;
+    for(let x = 0; x < userAnimeTitles.length; x+=25) {
+        message.channel.send(userAnimeTitles.slice(x,x+25))
+    }
 }
   
 module.exports = {
     name: 'user',
     description: 'returns info about mal username',
     async execute (message, payload){
-        const user = await getAnimeUser(payload);
-        for(let x = 0; x < user.length; x+=25) {
-            message.channel.send(user.slice(x,x+25))
-        }
+        getAnimeUser(message,payload);
     }
 }
